fba902f79237f671746e598f20313e08232a4197
DinaTaklit <ez_taklit@esi.dz>
2020-04-16 17:38:00 +0100

    feat: Impelemnt Card componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Create card component 
+
+class Card extends React.Component{
+  render(){
+    return(
+      <div className="github-profile">  
+        <img src="https://placehold.it/75"/> 
+        <div className="info">
+          <div className="name"> Name here ...</div>
+          <div className="company"> Company here..</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
 class App extends Component {
   render() {
     return (
       <div>
-        Github Cards APP
+        <div className="header">{this.props.title}</div>
+        <Card />
       </div>
-  
     );
   }
 }
