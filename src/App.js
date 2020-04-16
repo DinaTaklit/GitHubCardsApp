0c38a703e849bd8a389da7adde56f104068c527f
DinaTaklit <ez_taklit@esi.dz>
2020-04-16 21:40:54 +0100

    feat: Add cardList component to wrap cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// Create card component 
+// Use this data for premier test  
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
 
+// Create card list component 
+const CardList = (props) => {
+  return(
+    <div>
+    {testData.map((profile)=>{
+      return(
+        <Card {...profile} />
+      )
+    })}
+  </div>
+  )
+}
+
+// Create card component 
 class Card extends React.Component{
   render(){
+    const profile = this.props;
+
     return(
       <div className="github-profile">  
-        <img src="https://placehold.it/75"/> 
+        <img src={profile.avatar_url}/> 
         <div className="info">
-          <div className="name"> Name here ...</div>
-          <div className="company"> Company here..</div>
+        <div className="name"> {profile.name}</div>
+        <div className="company"> {profile.company}</div>
         </div>
       </div>
     );
@@ -18,13 +39,12 @@ class Card extends React.Component{
 }
 
 
-
 class App extends Component {
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <CardList />
       </div>
     );
   }
